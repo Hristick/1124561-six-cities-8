2007ec71e1211275f5647599b131f84eccb3e621
Hristick <hristick@mail.ru>
2021-10-03 18:04:53 +0300

    module2-task2

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import RenderMain from '../render-main/render-main';
+
+type AppProps = {
+  countOffer: number,
+}
+
+function App({countOffer}: AppProps): JSX.Element {
+  return (
+    <RenderMain countOffer = {countOffer}/>
+  );
 }
 
 export default App;
